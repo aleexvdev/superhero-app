c027b2ada2591d8c4f7879258f5caf8cd9060ee8
aleexvdev <valverdealex25@gmail.com>
2023-12-14 20:43:16 -0500

    modified token

--- src/api/apihero.tsx
+++ src/api/apihero.tsx
@@ -2,28 +2,10 @@ import axios, { AxiosResponse } from 'axios';
 import { TypeFetchAppResponse, TypeHeroApp } from '../types/TypeHero';
 
 const BASE_URL = 'https://superheroapi.com/api.php';
-const TOKEN = '1024982038525332';
-
-/* export const getHeroData2 =async (id: string): Promise<TypeFetchAppResponse> => {
-  try {
-    const url = `${BASE_URL}/${TOKEN}/${id}`;
-    const response: AxiosResponse<TypeHeroApp> = await axios.get(url);
-    return {
-      data: response.data,
-      message: 'Data fetched successfully',
-      status: 200
-    }
-  } catch (error) {
-    return {
-      data: [],
-      message: 'Failed to fetch data',
-      status: 500
-    }
-  }
-} */
+// const TOKEN = '1024982038525332';
 
 export const getHeroData = async (id: string): Promise<TypeFetchAppResponse> => {
-  const url = `${BASE_URL}/${TOKEN}/${id}`;
+  const url = `${BASE_URL}/1024982038525332/${id}`;
   try {
     const response: AxiosResponse<TypeHeroApp> = await axios.get(url);
     return {
